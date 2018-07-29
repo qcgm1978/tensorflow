ac0b625dcafc430f4d8bec952ce2d08d67a90e9e
qcgm1978 <zhanghl@strongunion.com.cn>
2018-07-29 16:46:30 +0800

    bug

--- scaffold.js
+++ scaffold.js
@@ -10,20 +10,33 @@ class ML {
      * @param  {array} def initial data set
      * @memberof ML
      */
-    constructor(formula, toLearn, def) {
+    constructor(formula, toLearn, def=[]) {
         this.formula = formula;
         this.toLearn = toLearn;
-        this.def = def;
-        const arr = this.getTolearnNum(formula, toLearn);
-        this.iniRandomArr = this.iniRandom(arr.length);
+        
+        var arr = this.getTolearnNum(formula, toLearn);
+        let arrIniCoffies = [];
+        if (arr.length === def.length) {
+            arrIniCoffies = def;
+        } else if (arr.length < def.length) {
+            arrIniCoffies = def.slice(0, arr.length)
+        } else {
+            const arrCoffies = new Array(arr.length - def.length);
+            const arrInit = arrCoffies.fill(0).map(item=>ML.getRandomArbitrary(-1, 1))
+            arrIniCoffies = def.concat(arrInit)
+        }
+        this.def = arrIniCoffies;
+        this.iniRandomArr = this.iniRandom(arrIniCoffies.length);
         this.iniCoeff = this.getArray(this.iniRandomArr);
         this.iniState()
         this.iniPredict();
         this.plot()
     }
-    static get NUM_POINTS() {
-        return 100;
+    /** * Returns a random number between min (inclusive) and max (exclusive) */
+    static getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
     }
+
     iniState() {
         this.training = this.generateData(ML.private.NUM_POINTS, this.def)
     }
@@ -33,7 +46,10 @@ class ML {
             y: this.training.y,
             mode: 'lines+markers',
             name: 'Training',
-            marker: { size: 12, color: '#29B6F6' }
+            marker: {
+                size: 12,
+                color: '#29B6F6'
+            }
         };
 
         const trace2 = {
@@ -41,7 +57,10 @@ class ML {
             y: this.prediction.y,
             mode: 'lines+markers',
             name: 'Initial Prediction',
-            marker: { size: 12, color: '#F06292' }
+            marker: {
+                size: 12,
+                color: '#F06292'
+            }
         };
 
         let trace3 = {};
@@ -51,28 +70,38 @@ class ML {
                 y: this.learning.y,
                 mode: 'lines+markers',
                 name: 'Learning',
-                marker: { size: 12, color: '#00E676' }
+                marker: {
+                    size: 12,
+                    color: '#00E676'
+                }
             };
         }
 
         const layout = {
             margin: {
-                l: 20, r: 0, b: 0, t: 0,
+                l: 20,
+                r: 0,
+                b: 0,
+                t: 0,
                 pad: 0
             },
             legend: {
                 xanchor: "left",
                 yanchor: "top",
-                y: 1,  //tbh i don't know what these numbers mean?!
+                y: 1,
+                //tbh i don't know what these numbers mean?!
                 x: 0,
                 orientation: "v"
             },
         };
-        Plotly.newPlot('graph', [trace1, trace2, trace3], layout, { displayModeBar: false });
+        Plotly.newPlot('graph', [trace1, trace2, trace3], layout, {
+            displayModeBar: false
+        });
     }
     getTolearnNum(formula, toLearn) {
         const len = formula.length;
-        const reg = new RegExp(toLearn, 'g'), arr = [];
+        const reg = new RegExp(toLearn,'g')
+          , arr = [];
         for (let i = 0; i < len; i++) {
             const match = reg.exec(formula);
             if (match) {
@@ -85,7 +114,7 @@ class ML {
         return arr;
     }
     getArray(arr) {
-        return arr.map(item => item.dataSync()[0])
+        return arr.map(item=>item.dataSync()[0])
     }
     iniRandom(len) {
         const arr = []
@@ -105,18 +134,23 @@ class ML {
         // 🚨 This is super important! TensorFlow requires this
         // for the algorithm to work, and if you don't do this
         // you learn NaN for every coefficient.
-        const xs = tf.randomUniform([points], -1, 1);  // magic TF to give you points between [-1, 1]
+        const xs = tf.randomUniform([points], -1, 1);
+        // magic TF to give you points between [-1, 1]
         for (let i = 0; i < points; i++) {
-            x[i] = xs.get(i);  // goes from a TF tensor (i.e. array) to a number.
+            x[i] = xs.get(i);
+            // goes from a TF tensor (i.e. array) to a number.
         }
-        x = x.sort(function (a, b) { return a - b })
+        x = x.sort(function(a, b) {
+            return a - b
+        })
 
         // Generate the random y values.  
         for (let i = 0; i < points; i++) {
             const val = x[i];
-            const strNoCoeff = arr.reduce((accumulator, item) => {
+            const strNoCoeff = arr.reduce((accumulator,item)=>{
                 return accumulator.replace(this.toLearn, item);
-            }, this.formula);
+            }
+            , this.formula);
             const formulaStr = strNoCoeff.replace(/x/g, `(${val})`);
             y[i] = math.eval(formulaStr);
         }
@@ -131,7 +165,10 @@ class ML {
             y[i] = (y[i] - ymin) / yrange;
         }
 
-        return { x: x, y: y }
+        return {
+            x: x,
+            y: y
+        }
     }
     async doALearning() {
         const that = this;
@@ -157,7 +194,7 @@ class ML {
         const optimizer = tf.train.sgd(learningRate);
 
         // Use the training data, and do numIteration passes over it. 
-        await train.call(this, tf.tensor1d(this.training.x), tf.tensor1d(this.training.y), numIterations);
+        await train.call(this,tf.tensor1d(this.training.x),tf.tensor1d(this.training.y),numIterations);
 
         // Once that is done, this has updated our coefficients! 
         // Here you could see what our predictions look like now, and use them!
@@ -183,11 +220,11 @@ class ML {
                 //     c: c.dataSync()[0],
                 //     d: d.dataSync()[0],
                 // };
-                this.learning = this.generateData(ML.private.NUM_POINTS, this.iniCoeff);
+                this.learning = this.generateData(ML.private.NUM_POINTS, this.getArray(this.iniRandomArr));
                 this.plot();
 
                 // Learn! This is where the step happens, and when the training takes place.
-                optimizer.minimize(() => {
+                optimizer.minimize(()=>{
                     // Using our estimated coeff, predict all the ys for all the xs 
                     const pred = predict(xs);
 
@@ -195,7 +232,8 @@ class ML {
                     // correct answer. The optimizer will then adjust the coefficients
                     // to minimize this loss.
                     return loss(pred, ys);
-                });
+                }
+                );
 
                 // Use tf.nextFrame to not block the browser.
                 await tf.nextFrame();
@@ -209,12 +247,11 @@ class ML {
             // Calculate a y according to the formula
             // y = a * x ^ 3 + b * x ^ 2 + c * x + d
             // where a, b, c, d are the coefficients we have currently calculated.
-            return tf.tidy(() => {
-                return that.iniRandomArr[0].mul(x.pow(tf.scalar(3, 'int32')))
-                    .add(that.iniRandomArr[1].mul(x.square()))
-                    .add(that.iniRandomArr[2].mul(x))
-                    .add(that.iniRandomArr[3]);
-            });
+            return tf.tidy(()=>{
+                const result = that.iniRandomArr[0].mul(x.pow(tf.scalar(3, 'int32'))).add(that.iniRandomArr[1].mul(x.square())).add(that.iniRandomArr[2].mul(x)).add(that.iniRandomArr[3]);
+                return result;
+            }
+            );
         }
 
         /*
@@ -238,5 +275,5 @@ ML.private = {
     NUM_POINTS: 100,
 
 }
-const ml = new ML('coef*x^3+coef*x^2+coef*x+coef', 'coef', [-0.8, -0.2, 0.9, 0.5]);
+const ml = new ML('coef*x^3+coef*x^2+coef*x+coef','coef',[-0.8, -0.2, 0.9]);
 const doALearning = ml.doALearning.bind(ml);
