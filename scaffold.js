d5886848358ee0f1a814dbb50be24e17d07007bd
qcgm1978 <zhanghl@strongunion.com.cn>
2018-07-29 18:01:12 +0800

    seperate seven steps

--- scaffold.js
+++ scaffold.js
@@ -13,31 +13,42 @@ class ML {
     constructor(formula, toLearn, def=[]) {
         this.formula = formula;
         this.toLearn = toLearn;
-        
+
         var arr = this.getTolearnNum(formula, toLearn);
+        this.def = this.getDef(arr, def);
+        this.execPattern()
+
+        this.plot()
+    }
+    execPattern() {
+        this.figureOutFeatures = this.getFeaturesEval;
+        this.iniPredict();
+        this.generateTrainTarget();
+        this.watchLoops = this.getCloser;
+        this.gengerateOptimizer();
+        this.sixthStep = this.calMetricDerivatives;
+        this.sevenFeedData=this.train;
+    }
+    getDef(arr, def) {
         let arrIniCoffies = [];
         if (arr.length === def.length) {
             arrIniCoffies = def;
         } else if (arr.length < def.length) {
-            arrIniCoffies = def.slice(0, arr.length)
+            arrIniCoffies = def.slice(0, arr.length);
         } else {
             const arrCoffies = new Array(arr.length - def.length);
-            const arrInit = arrCoffies.fill(0).map(item=>ML.getRandomArbitrary(-1, 1))
-            arrIniCoffies = def.concat(arrInit)
+            const arrInit = arrCoffies.fill(0).map(item=>ML.getRandomArbitrary(-1, 1));
+            arrIniCoffies = def.concat(arrInit);
         }
-        this.def = arrIniCoffies;
-        this.iniRandomArr = this.iniRandom(arrIniCoffies.length);
-        this.iniCoeff = this.getArray(this.iniRandomArr);
-        this.iniState()
-        this.iniPredict();
-        this.plot()
+        return arrIniCoffies;
     }
+
     /** * Returns a random number between min (inclusive) and max (exclusive) */
     static getRandomArbitrary(min, max) {
         return Math.random() * (max - min) + min;
     }
 
-    iniState() {
+    generateTrainTarget() {
         this.training = this.generateData(ML.private.NUM_POINTS, this.def)
     }
     plot() {
@@ -124,6 +135,8 @@ class ML {
         return arr;
     }
     iniPredict() {
+        this.iniRandomArr = this.iniRandom(this.def.length);
+        this.iniCoeff = this.getArray(this.iniRandomArr);
         this.prediction = this.generateData(ML.private.NUM_POINTS, this.iniCoeff)
     }
     generateData(points, arr) {
@@ -147,12 +160,7 @@ class ML {
         // Generate the random y values.  
         for (let i = 0; i < points; i++) {
             const val = x[i];
-            const strNoCoeff = arr.reduce((accumulator,item)=>{
-                return accumulator.replace(this.toLearn, item);
-            }
-            , this.formula);
-            const formulaStr = strNoCoeff.replace(/x/g, `(${val})`);
-            y[i] = math.eval(formulaStr);
+            y[i] = this.figureOutFeatures(arr, val);
         }
 
         // Normalize the y values to be between 0 and 1
@@ -170,6 +178,15 @@ class ML {
             y: y
         }
     }
+    getFeaturesEval(arr, val) {
+        const strNoCoeff = arr.reduce((accumulator,item)=>{
+            return accumulator.replace(this.toLearn, item);
+        }
+        , this.formula);
+        const formulaStr = strNoCoeff.replace(/x/g, `(${val})`);
+        return math.eval(formulaStr);
+    }
+
     async doALearning() {
         const that = this;
         // Create an optimizer. This is the thing that does the learning.
@@ -179,22 +196,8 @@ class ML {
         // How many passes through the data we're doing.
         const numIterations = parseInt(document.getElementById('iterations').value || 75);
 
-        // How fast we are learning.
-        const learningRate = 0.5;
-
-        /* 
-          Docs: https://js.tensorflow.org/api/0.11.1/#train.sgd
-          - sgd means "stochastic gradient descent". 
-          - stochastic means "probabilistic"
-          - gradient descent means that at every step, we move our predictions 
-          in the direction of the answer. How much to move involves derivatives (the "gradient")
-          - the full algorithm is here but it's...mathy: https://en.wikipedia.org/wiki/Stochastic_gradient_descent
-          - this is why having tensorflow is good!!
-        */
-        const optimizer = tf.train.sgd(learningRate);
-
         // Use the training data, and do numIteration passes over it. 
-        await train.call(this,tf.tensor1d(this.training.x),tf.tensor1d(this.training.y),numIterations);
+        await this.sevenFeedData.call(this,tf.tensor1d(this.training.x),tf.tensor1d(this.training.y),numIterations);
 
         // Once that is done, this has updated our coefficients! 
         // Here you could see what our predictions look like now, and use them!
@@ -208,72 +211,92 @@ class ML {
         // this.prediction = generateData(NUM_POINTS, coeff);
         // plot();
 
-        /*
+    }
+    /*
          * This does the training of the model.
          */
-        async function train(xs, ys, numIterations) {
-            for (let iter = 0; iter < numIterations; iter++) {
-                // Plot where we are at this step.
-                // const coeff = {
-                //     a: a.dataSync()[0],
-                //     b: b.dataSync()[0],
-                //     c: c.dataSync()[0],
-                //     d: d.dataSync()[0],
-                // };
-                this.learning = this.generateData(ML.private.NUM_POINTS, this.getArray(this.iniRandomArr));
-                this.plot();
+    async train(xs, ys, numIterations) {
+        for (let iter = 0; iter < numIterations; iter++) {
+            // Plot where we are at this step.
+            // const coeff = {
+            //     a: a.dataSync()[0],
+            //     b: b.dataSync()[0],
+            //     c: c.dataSync()[0],
+            //     d: d.dataSync()[0],
+            // };
+            this.sixthStep(xs, ys);
 
-                // Learn! This is where the step happens, and when the training takes place.
-                optimizer.minimize(()=>{
-                    // Using our estimated coeff, predict all the ys for all the xs 
-                    const pred = predict(xs);
-
-                    // Need to return the loss i.e how bad is our prediction from the 
-                    // correct answer. The optimizer will then adjust the coefficients
-                    // to minimize this loss.
-                    return loss(pred, ys);
-                }
-                );
-
-                // Use tf.nextFrame to not block the browser.
-                await tf.nextFrame();
-            }
+            // Use tf.nextFrame to not block the browser.
+            await tf.nextFrame();
         }
-
-        /*
+    }
+    /*
          * Predicts all the y values for all the x values.
          */
-        function predict(x) {
-            // Calculate a y according to the formula
-            // y = a * x ^ 3 + b * x ^ 2 + c * x + d
-            // where a, b, c, d are the coefficients we have currently calculated.
-            return tf.tidy(()=>{
-                const result = that.iniRandomArr[0].mul(x.pow(tf.scalar(3, 'int32'))).add(that.iniRandomArr[1].mul(x.square())).add(that.iniRandomArr[2].mul(x)).add(that.iniRandomArr[3]);
-                return result;
-            }
-            );
+    predict(x) {
+        const that = this;
+        // Calculate a y according to the formula
+        // y = a * x ^ 3 + b * x ^ 2 + c * x + d
+        // where a, b, c, d are the coefficients we have currently calculated.
+        return tf.tidy(()=>{
+            const result = that.iniRandomArr[0].mul(x.pow(tf.scalar(3, 'int32'))).add(that.iniRandomArr[1].mul(x.square())).add(that.iniRandomArr[2].mul(x)).add(that.iniRandomArr[3]);
+            return result;
         }
+        );
+    }
 
-        /*
+    /*
          * Loss function: how good the prediction is based on what you expected.
          */
-        function loss(prediction, labels) {
-            // This is the mean squared error between the prediction and the correct answer
-            // If you had n data points (NUM_POINTS = n) then it would be:
-            // error = 1/n * ( (prediction_1 - answer_1)^2 + ... + (prediction_n - answer_n)^2  )
-            // see https://en.wikipedia.org/wiki/Mean_squared_error.
-            // There are other error functions you could use, but if you're doing numeric things,
-            // MSE is one of the best and also the easiest.
+    loss(prediction, labels) {
+        // This is the mean squared error between the prediction and the correct answer
+        // If you had n data points (NUM_POINTS = n) then it would be:
+        // error = 1/n * ( (prediction_1 - answer_1)^2 + ... + (prediction_n - answer_n)^2  )
+        // see https://en.wikipedia.org/wiki/Mean_squared_error.
+        // There are other error functions you could use, but if you're doing numeric things,
+        // MSE is one of the best and also the easiest.
 
-            // Also, this is also why TensorFlow is great! Doing this by hand sucks!
-            const error = prediction.sub(labels).square().mean();
-            return error;
+        // Also, this is also why TensorFlow is great! Doing this by hand sucks!
+        const error = prediction.sub(labels).square().mean();
+        return error;
+    }
+    calMetricDerivatives(xs, ys) {
+        this.learning = this.generateData(ML.private.NUM_POINTS, this.getArray(this.iniRandomArr));
+        this.plot();
+        // Learn! This is where the step happens, and when the training takes place.
+        this.watchLoops(xs, ys);
+    }
+
+    gengerateOptimizer() {
+        // How fast we are learning.
+        const learningRate = 0.5;
+        /*
+          Docs: https://js.tensorflow.org/api/0.11.1/#train.sgd
+          - sgd means "stochastic gradient descent".
+          - stochastic means "probabilistic"
+          - gradient descent means that at every step, we move our predictions
+          in the direction of the answer. How much to move involves derivatives (the "gradient")
+          - the full algorithm is here but it's...mathy: https://en.wikipedia.org/wiki/Stochastic_gradient_descent
+          - this is why having tensorflow is good!!
+        */
+        this.optimizer = tf.train.sgd(learningRate);
+    }
+
+    getCloser(xs, ys) {
+        this.optimizer.minimize(()=>{
+            // Using our estimated coeff, predict all the ys for all the xs 
+            const pred = this.predict(xs);
+            // Need to return the loss i.e how bad is our prediction from the 
+            // correct answer. The optimizer will then adjust the coefficients
+            // to minimize this loss.
+            return this.loss(pred, ys);
         }
+        );
     }
 }
 ML.private = {
     NUM_POINTS: 100,
 
 }
-const ml = new ML('coef*x^3+coef*x^2+coef*x+coef','coef',[-0.8, -0.2, 0.9]);
+const ml = new ML('coef*x^3+coef*x^2+coef*x+coef','coef',[-0.8, -0.2, 0.9, 0.7, -0.5]);
 const doALearning = ml.doALearning.bind(ml);
